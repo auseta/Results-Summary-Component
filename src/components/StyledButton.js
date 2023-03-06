c405976320ab1fb5e5af784c58d8aed2c7bd4b96
Augusto Seta <alseta97@gmail.com>
2023-03-05 23:58:55 -0300

    Styled Button Created
    Created a button with style components

---
+++ src/components/StyledButton.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  margin: 20px 0;
+  font-family: 'Hanken Grotesk';
+  font-weight: 700;
+  width: 82%;
+  padding: 15px 0;
+  border: none;
+  border-radius: 30px;
+  background-color: #303b59;
+  color: #ffffff;
+  font-size: 15px;
+`
