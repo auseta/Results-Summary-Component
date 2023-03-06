921e0644dcdb493a9db7a125f55dd453d7290459
Augusto Seta <alseta97@gmail.com>
2023-03-06 02:44:43 -0300

    Changes in data.json
    The data.json file extension was changed to a javascript extension to contain a variable with an array of objects.

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import ReactionIcon from '../images/icon-reaction.svg'
+import MemoryIcon from '../images/icon-memory.svg'
+import VerbalIcon from '../images/icon-verbal.svg'
+import VisualIcon from '../images/icon-visual.svg'
+
+const data = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": ReactionIcon
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": MemoryIcon
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": VerbalIcon
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": VisualIcon
+  }
+]
+
+export default data;
