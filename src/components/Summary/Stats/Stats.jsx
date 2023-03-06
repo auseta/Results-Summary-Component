5239e1f711ee16763692c6c9a1fd2c76c6b2983f
Augusto Seta <alseta97@gmail.com>
2023-03-06 02:21:02 -0300

    Changes in Stats.jsx
    The name of the data.json import variable was changed.

--- src/components/Summary/Stats/Stats.jsx
+++ src/components/Summary/Stats/Stats.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import data from '../../../data/data.json'
+import Data from '../../../data/data.json'
 import { StyledCategoryContainer, StyledScoreContainer, StyledStatContainer, StyledStatsContainer } from './StyledStats'
 
 const Stats = () => {
   return (
     <StyledStatsContainer>
       {
-        data.map(dt => (
+        Data.map(dt => (
           <StyledStatContainer key={dt.category} category={dt.category} >
             <StyledCategoryContainer category={dt.category} >
               <img src={dt.icon} alt={`${dt.category}-stat`} />
